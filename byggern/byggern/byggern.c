22209d987fc557a4626617d9c6f2474c54e11456
tommp <tompersen@gmail.com>
2013-08-29 12:13:26 +0200

    Atmel studio 6 initial setup

---
+++ byggern/byggern/byggern.c
@@ -0,0 +1,17 @@
+/*
+ * byggern.c
+ *
+ * Created: 29.08.2013 12:12:10
+ *  Author: tommp
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
