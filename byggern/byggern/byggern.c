4c27d8b94bcbf56eee5c838072a7c4a325e75a1d
tommp <tompersen@gmail.com>
2013-08-29 15:56:28 +0200

    light-show is now working

--- byggern/byggern/byggern.c
+++ byggern/byggern/byggern.c
@@ -5,16 +5,33 @@
  *  Author: tommp
  */ 
 
-
+#include <util/delay.h>
 #include <avr/io.h>
 
+	void Wait()
+	{
+		for(uint8_t i=0;i<2;i++)
+		{
+			_delay_loop_2(0);
+		}
+	}
+	
 int main(void)
 {
-	DDRB |= (1 << DDB1);
+	DDRB = 0xFF;
+	PORTB = 0x80;
     while(1)
     {
-		PORTB |= (1 << DDB1);
-		PORTB &= ~(1 << DDB1);
+		for (int i = 0; i < 8; i++){
+			PORTB = (PORTB >> 1);
+			
+			if(i == 0){
+				PORTB |= (1 << DDB7);
+			}
+			Wait();
+		}
+		
+		
         //TODO:: Please write your application code 
     }
 }
