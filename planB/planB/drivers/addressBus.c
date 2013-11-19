6a2c059829a781b339d10b769f6289aa9a16bf9b
Magnus Bae <magnusbae@gmail.com>
2013-11-19 20:54:25 +0100

    Removed unused stuff

--- planB/planB/drivers/addressBus.c
+++
@@ -1,8 +0,0 @@
-#include "addressBus.h"
-#include <avr/io.h>
-
-
-void initialAddressBusSetup(){
-	XMCRA |= (1<<SRE); //Enables the External Memory Interface.The pin functions AD7:0, A15:8, ALE, WR, and RD are activated as the alternate pin functions.
-	XMCRB |= (1<<XMM2) | (1<<XMM1); //Release pins PC7 to PC2 for normal use, retain PC0 and PC1 for address bus
-}
